0d04bb3659fd53f661f8fc5708e812adff744744
ysthakur <2004yst@gmail.com>
2022-04-27 15:41:23 +0000

    Add home, signup, and signin pages to flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,19 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+def homepage():
+    return render_template("index.html")
+
+@app.route('/signup')
+def signup():
+    return render_template("signup.html")
+
+@app.route('/signin')
+def signin():
+    return render_template("signin.html")
 
