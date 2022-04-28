160ad060d898f2edf5f590b81fd4f10801067993
ysthakur <2004yst@gmail.com>
2022-04-28 13:20:31 +0000

    Save work

--- flask_app.py
+++ flask_app.py
@@ -1,28 +1,32 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask, render_template, current_user
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnecttor://{hwplan}:{passphrase}@hwplan.mysql.pythonanywhere-services.com/hwplan$default"
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnecttor://hwplan:passphrase@hwplan.mysql.pythonanywhere-services.com/hwplan$default"
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=["GET", "POST"])
 def index():
-    if current_user.is_authenticated:
     return render_template("index.html")
 
 @app.route('/signup')
 def signup():
     return render_template("signup.html")
 
-@app.route('/signin')
+@app.route('/signin', methods=['GET', 'POST'])
 def signin():
-    return render_template("signin.html")
+    if request.method == "GET":
+        return render_template("signin.html", error=False)
+    elif request.form["username"] != "admin" or request.form["password"] != "secret":
+        return render_template("signin.html", error=True)
+    else:
+        return redirect(url_for("signedin"))
 
 @app.route('/signedin')
+def signedin():
     return render_template("signedin.html")
-
