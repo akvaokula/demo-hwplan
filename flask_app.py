f1a0fa9511bcfefddf56806449a4c7a1dfd5aef4
ysthakur <2004yst@gmail.com>
2022-04-28 12:29:14 +0000

    Start on database stuff

--- flask_app.py
+++ flask_app.py
@@ -4,11 +4,14 @@
 from flask import Flask, render_template, current_user
 
 app = Flask(__name__)
-
 app.config['DEBUG'] = True
 
-@app.route('/')
-def homepage():
+SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnecttor://{hwplan}:{passphrase}@hwplan.mysql.pythonanywhere-services.com/hwplan$default"
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
     if current_user.is_authenticated:
     return render_template("index.html")
 
