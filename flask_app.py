2ed4781b238aec8faa320dc37295fb575ee33986
ysthakur <2004yst@gmail.com>
2022-04-28 12:15:09 +0000

    Add signed in page

--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,15 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, render_template
+from flask import Flask, render_template, current_user
 
 app = Flask(__name__)
 
+app.config['DEBUG'] = True
+
 @app.route('/')
 def homepage():
+    if current_user.is_authenticated:
     return render_template("index.html")
 
 @app.route('/signup')
@@ -16,4 +19,7 @@ def signup():
 @app.route('/signin')
 def signin():
     return render_template("signin.html")
+
+@app.route('/signedin')
+    return render_template("signedin.html")
 
